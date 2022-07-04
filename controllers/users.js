f2a7fdee4746cf08ea287a0c42ab30385ba41e2a
KlausMikhaelson <satyamsingh5076@gmail.com>
2022-07-05 02:28:27 +0530

    added controllers and made routes look nicer

--- controllers/users.js
+++ controllers/users.js
@@ -1,28 +1,30 @@
-import express from "express";
-import { createUser } from "../controllers/users.js";
 import { v4 as uuidv4 } from "uuid";
 
-const router = express.Router();
 
-let users =[]
+export const createUser = (req, res) => {
 
-// all routes are starting with /users
+    const user = req.body;
+    // since they are being used only once
+    // const userId = uuidv4();
+    // const userWithId = { ...user, id: uuidv4()}
+    users.push( { ...user, id: uuidv4()})
 
-router.get('/', (req, res) => {
+    res.send(`User with the username ${user.firstName} added to the database`)
+}
+
+export const getUser =  (req, res) => {
     res.send(users)
-});
+}
 
-router.post('/', createUser);
-
-router.get('/:id', (req, res) => {
+export const getId =  (req, res) => {
     const { id } = req.params;
 
     const foundUser = users.find((user) => user.id === id);
 
     res.send(foundUser);
-})
+};
 
-router.delete('/:id', (req, res) => {
+export const deleteId = (req, res) => {
     const { id } = req.params;
     //id to delete 123
     //Elijah 123
@@ -30,9 +32,9 @@ router.delete('/:id', (req, res) => {
     users = users.filter((user) => user.id !== id);
 
     res.send(`User with the id ${id} deleted frpm the database.`)
-})
+}
 
-router.patch(':id', (req, res) => {
+export const patchId = (req, res) => {
     const { id } = req.params;
     const { firstName, lastName, age } = req.body;
     const userTobeUpdated = users.find((user) => user.id === id);
@@ -50,7 +52,4 @@ router.patch(':id', (req, res) => {
     res.send(`User with the id ${id} has been updated`)
 
 
-})
-
-
-export default router;
+};
